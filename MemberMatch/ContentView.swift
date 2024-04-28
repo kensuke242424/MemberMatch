7ea9415636d94c3aaa99f353349013043f99f8be
Kensuke Nakagawa <kennsuke242424@gmail.com>
2024-04-28 21:14:46 +0900

    first commit.

---
+++ MemberMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemberMatch
+//
+//  Created by Kensuke Nakagawa on 2024/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemberMatch/MemberMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemberMatchApp.swift
+//  MemberMatch
+//
+//  Created by Kensuke Nakagawa on 2024/04/28.
+//
+
+import SwiftUI
+
+@main
+struct MemberMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
